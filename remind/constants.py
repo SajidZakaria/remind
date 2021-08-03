7d07b57bb4f1ecb43c9dc92ac65139123210ee6e
Sajid Zakaria <sajid88@student.sust.edu>
2021-08-03 15:09:01 +0600

    add SUPER_USERS

--- remind/constants.py
+++ remind/constants.py
@@ -7,5 +7,5 @@ LOG_FILE_PATH = os.path.join(LOGS_DIR, 'remind.log')
 GUILD_SETTINGS_MAP_PATH = os.path.join(DATA_DIR, 'guild_settings_map')
 ALL_DIRS = (attrib_value for attrib_name, attrib_value in list(
     globals().items()) if attrib_name.endswith('DIR'))
-SUPER_USERS = []
+SUPER_USERS = [49859, 49860, 49858]
 REMIND_MODERATOR_ROLE = "Remind Moderator"
